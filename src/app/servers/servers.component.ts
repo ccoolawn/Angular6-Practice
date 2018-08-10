c2a34882da84d8f095f04ec0d5bc9a69f1fb5609
Cornell Coulon <ccoulon@cornellcoulon.com>
2018-08-10 18:41:50 -0500

    Updated directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,57 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  template:`
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template:`
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  serverCreated = false;
+  allowNewUser = false;
+  userCreationStatus = 'No user was created!';
+  userName = '';
+  // newUserName = '';
 
-  constructor() { }
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! The new server name is: ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onCreateUser() {
+    this.userCreationStatus = 'Hello, ' + this.userName + ' welcome to the company!';
+  }
+
+  onUpdateUserName(event: any) {
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
+
+  resetUser() {
+    this.userName = '';
+  }
+
+// This Method makes multiple calls
+  buttonEvent() {
+    this.onCreateUser();
+    this.resetUser();
+  }
 }
